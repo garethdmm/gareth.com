c8ecc879b58cd41cb6801f8e66db15c7c9f3345d
gareth <gareth@PaloAlto.(none)>
2011-05-23 15:25:38 -0400

    Rebuilt the new design after our git meltdown.
    This is a checkpoint with text-only, no graphics.

--- js/tips.js
+++ js/tips.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $(".tip-able").tooltip({
     delay: 500,
     effect: 'fade',
-    position: 'bottom center',
+    position: 'center right',
     events: {
       def: 'click, mouseout'
     }
